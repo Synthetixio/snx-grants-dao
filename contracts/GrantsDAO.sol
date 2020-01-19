070e566e6b21f730bc7c5ec1752dafbe1bf66931
Thomas Hodges <me@thodges.com>
2020-01-18 19:49:53 -0600

    Separate team and community signers

--- contracts/GrantsDAO.sol
+++ contracts/GrantsDAO.sol
@@ -2,11 +2,18 @@ pragma solidity 0.5.13;
 
 contract GrantsDAO {
 
-  mapping(address => bool) public signers;
+  mapping(address => bool) public teamSigners;
+  mapping(address => bool) public communitySigners;
 
-  constructor(address[] memory _signers) public {
-    for (uint i = 0; i < _signers.length; i++) {
-      signers[_signers[i]] = true;
+  constructor(
+    address[] memory _teamSigners,
+    address[] memory _communitySigners
+  ) public {
+    for (uint i = 0; i < _teamSigners.length; i++) {
+      teamSigners[_teamSigners[i]] = true;
+    }
+    for (uint i = 0; i < _communitySigners.length; i++) {
+      communitySigners[_communitySigners[i]] = true;
     }
   }
 }
--- test/GrantsDAO_test.js
+++ test/GrantsDAO_test.js
@@ -2,22 +2,28 @@ contract('GrantsDAO', (accounts) => {
   const GrantsDAO = artifacts.require('GrantsDAO')
 
   const defaultAccount = accounts[0]
-  const signer1 = accounts[1]
-  const signer2 = accounts[2]
-  const signer3 = accounts[3]
-  const signer4 = accounts[4]
-  const signer5 = accounts[5]
-  const signers = [signer1, signer2, signer3, signer4, signer5]
+  const teamSigner1 = accounts[1]
+  const teamSigner2 = accounts[2]
+  const communitySigner1 = accounts[3]
+  const communitySigner2 = accounts[4]
+  const communitySigner3 = accounts[5]
+  const teamSigners = [teamSigner1, teamSigner2]
+  const communitySigners = [communitySigner1, communitySigner2, communitySigner3]
 
   let dao
 
   beforeEach(async () => {
-    dao = await GrantsDAO.new(signers, { from: defaultAccount })
+    dao = await GrantsDAO.new(
+      teamSigners,
+      communitySigners,
+      { from: defaultAccount }
+    )
   })
 
   describe('constructor', () => {
     it('deploys with the specified addresses as signers', async () => {
-      signers.forEach(async s => assert.isTrue(await dao.signers.call(s)))
+      teamSigners.forEach(async s => assert.isTrue(await dao.teamSigners.call(s)))
+      communitySigners.forEach(async s => assert.isTrue(await dao.communitySigners.call(s)))
     })
   })
 })
