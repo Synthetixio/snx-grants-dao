918139834e57f74a2a53d3b64ce6b92fe61c227f
raymogg <raymondmogg@gmail.com>
2020-02-04 18:42:38 +1000

    Updated migrations and network configs

--- migrations/2_grantsdao_migration.js
+++ migrations/2_grantsdao_migration.js
@@ -8,28 +8,60 @@ module.exports = async (deployer, network, accounts) => {
   const tokenDecimals = new BN(18)
   const tokenInitialSupply = web3.utils.toWei('1000')
   const toPass = new BN(4)
-  const teamMembers = [accounts[1], accounts[2]]
-  const communityMembers = [accounts[3], accounts[4], accounts[5]]
-  await deployer.deploy(
-    MockToken,
-    tokenName,
-    tokenSymbol,
-    tokenDecimals,
-    tokenInitialSupply,
-  )
 
-  await deployer.deploy(
-    GrantsDAO,
-    MockToken.address,
-    teamMembers,
-    communityMembers,
-    toPass,
-  )
+  var teamMembers;
+  var communityMembers;
 
-  deployedInstance = await GrantsDAO.deployed()
-  deployedToken = await MockToken.deployed()
-  await deployedToken.transfer(GrantsDAO.address, web3.utils.toWei("1000"))
-  await deployedInstance.createProposal(accounts[0], web3.utils.toWei("50"), "Example Proposal", "testurl.com", {from: accounts[1]})
-  //await deployedInstance.voteProposal(1, true, {from: accounts[2]})
-  console.log("Test Data Injected")
+  //All Non main network deployments
+  if (network == "main") {
+    //Define team members and community members
+
+    //Use SNX mainnet token
+    var tokenAddress = "0xc011a72400e58ecd99ee497cf89e3775d4bd732f"
+
+    await deployer.deploy(
+      GrantsDAO,
+      tokenAddress,
+      teamMembers,
+      communityMembers,
+      toPass,
+    )
+
+  } else {
+    //All other deployments
+    if (network == "dev") {
+      teamMembers = [accounts[1], accounts[2]]
+      communityMembers = [accounts[3], accounts[4], accounts[5]]
+    } else {
+      //TODO Inject real accounts
+      teamMembers = [accounts[1], accounts[2]]
+      communityMembers = [accounts[3], accounts[4], accounts[5]]
+    }
+    await deployer.deploy(
+      MockToken,
+      tokenName,
+      tokenSymbol,
+      tokenDecimals,
+      tokenInitialSupply,
+    )
+
+    await deployer.deploy(
+      GrantsDAO,
+      MockToken.address,
+      teamMembers,
+      communityMembers,
+      toPass,
+    )
+
+    if (network == "dev") {
+      //Inject mock data for easier testing
+      deployedInstance = await GrantsDAO.deployed()
+      deployedToken = await MockToken.deployed()
+      await deployedToken.transfer(GrantsDAO.address, web3.utils.toWei("1000"))
+      await deployedInstance.createProposal(accounts[0], web3.utils.toWei("50"), "Example Proposal", "testurl.com", { from: accounts[1] })
+      //await deployedInstance.voteProposal(1, true, {from: accounts[2]})
+      console.log("Test Data Injected")
+    }
+
+  }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,23 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+var mnemonic = "lyrics various math speak almost tonight license crash whisper flush gossip knee";
+
 module.exports = {
   networks: {
     dev: {
      host: "127.0.0.1",
      port: 8545,
      network_id: "*",
+    },
+    ropsten: {
+      provider: () =>
+      new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/4c85724d14d44bd0a579811c1e467d38"),
+      network_id: 3
+    },
+
+    main: {
+      provider: () =>
+      new HDWalletProvider(mnemonic, "https://infura.io/v3/4c85724d14d44bd0a579811c1e467d38"),
+      network_id: 3
     }
   },
   compilers: {
