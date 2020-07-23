cb8435f33c67ee2779485804e10052d79c36df58
Ramiro Vazquez <ramirovazquez@gmail.com>
2020-07-23 11:04:23 -0300

    fix icon rendering bug on load

--- dapp/gatsby-browser.js
+++ dapp/gatsby-browser.js
@@ -1,8 +1,13 @@
 import React from "react"
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
 import fetch from "isomorphic-fetch"
-
 import Layout from "./src/components/layout"
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import "@fortawesome/fontawesome-svg-core/styles.css"
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from "@fortawesome/fontawesome-svg-core"
+config.autoAddCss = false /* eslint-disable import/first */
 
 const cache = new InMemoryCache()
 
